310b48a935d80fffe1e07dd83008a33c06df0a6e
Sogan87 <sandro.leonardo.ti@gmail.com>
2023-05-22 15:29:02 -0300

    Seleção individual

--- src/controllers/carroController.js
+++ src/controllers/carroController.js
@@ -14,5 +14,14 @@ module.exports = {
             });
         }
         res.json(json);
+    },
+    buscarUm: async (req, res) =>{
+        let json = {error: '', result:{}};
+        let codigo = req.params.codigo;
+        let carro = await carroService.buscarUm(codigo);
+        if(carro){
+            json.result = carro;
+        }
+        res.json(json);
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const carroController = require('./controllers/carroController');
 
 router.get('/carros', carroController.buscarTodos);
+router.get('/carro/:codigo', carroController.buscarUm);
 
 module.exports = router;

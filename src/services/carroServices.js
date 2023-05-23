4c3adffbe6aaeec2d9bd0e12ff2aa4070ffd62b6
Sogan87 <sandro.leonardo.ti@gmail.com>
2023-05-23 09:39:06 -0300

    Atualiza o codigo automaticamente

--- src/services/carroServices.js
+++ src/services/carroServices.js
@@ -44,12 +44,58 @@ module.exports={
             );
         });
     },
-    excluir: (codigo)=> {
-        return new Promise((aceito, rejeitado)=> {
-            db.query('DELETE FROM carros WHERE codigo = ?',[codigo], (error, results ) =>{
-                if(error){ rejeitado(error); return; }
-                aceito(results);
+    excluir: (codigo) => {
+        return new Promise((aceito, rejeitado) => {
+            db.query('DELETE FROM carros WHERE codigo = ?', [codigo], (error, results) => {
+                if (error) {
+                    rejeitado(error);
+                    return;
+                }
+    
+                if (results.affectedRows > 0) {
+                    // Selecionar os itens subsequentes ao item excluído
+                    db.query('SELECT * FROM carros WHERE codigo > ? ORDER BY codigo', [codigo], (error, results) => {
+                        if (error) {
+                            rejeitado(error);
+                            return;
+                        }
+    
+                        const itensSubsequentes = results;
+    
+                        if (itensSubsequentes.length > 0) {
+                            let novoCodigo = codigo;
+    
+                            // Atualizar os códigos identificadores dos itens subsequentes
+                            const atualizarCodigo = (indice) => {
+                                if (indice >= itensSubsequentes.length) {
+                                    // Todos os códigos foram atualizados
+                                    aceito(results);
+                                    return;
+                                }
+    
+                                const item = itensSubsequentes[indice];
+    
+                                db.query('UPDATE carros SET codigo = ? WHERE codigo = ?', [novoCodigo, item.codigo], (error, results) => {
+                                    if (error) {
+                                        rejeitado(error);
+                                        return;
+                                    }
+    
+                                    novoCodigo++;
+                                    atualizarCodigo(indice + 1);
+                                });
+                            };
+    
+                            atualizarCodigo(0);
+                        } else {
+                            aceito(results);
+                        }
+                    });
+                } else {
+                    aceito(results);
+                }
             });
         });
     }
+    
 };

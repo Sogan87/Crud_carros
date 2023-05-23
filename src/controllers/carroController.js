0b1d91b108964fe978000ffa5536b434bc3f5205
Sogan87 <sandro.leonardo.ti@gmail.com>
2023-05-23 09:33:10 -0300

    Antes do frontend

--- src/controllers/carroController.js
+++ src/controllers/carroController.js
@@ -23,5 +23,50 @@ module.exports = {
             json.result = carro;
         }
         res.json(json);
-    }
+    },
+    inserir: async(req, res) => {
+        let json = {error:'', result:{}};
+
+        let modelo = req.body.modelo;
+        let placa = req.body.placa;
+
+        if (modelo && placa){
+            let carroCodigo = await carroService.inserir(modelo, placa);
+            json.result = {
+                codigo: carroCodigo,
+                modelo,
+                placa
+            };
+        }else{
+            json.error = 'Campos não enviados';
+        }
+        res.json(json);
+    },
+    alterar: async(req, res) => {
+        let json = {error:'', result:{}};
+
+        let codigo = req.params.codigo;
+        let modelo = req.body.modelo;
+        let placa = req.body.placa;
+
+        if (codigo && modelo && placa){
+            await carroService.alterar(codigo, modelo, placa);
+            json.result = {
+                codigo,
+                modelo,
+                placa
+            };
+        }else{
+            json.error = 'Campos não enviados';
+        }
+        res.json(json);
+    },
+    excluir: async(req, res) => {
+        let json = {error:'', result:{}};
+
+        await carroService.excluir(req.params.codigo);
+        
+        res.json(json);
+    },
+
 }
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,8 @@ const carroController = require('./controllers/carroController');
 
 router.get('/carros', carroController.buscarTodos);
 router.get('/carro/:codigo', carroController.buscarUm);
+router.post('/carro', carroController.inserir);
+router.put('/carro/:codigo', carroController.alterar);
+router.delete('/carro/:codigo', carroController.excluir);
 
 module.exports = router;
